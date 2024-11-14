0eb44d48eb8c793c0d3b0a6e450e8b7006bf65a3
GRFerri <gabrielferri27@gmail.com>
2024-11-14 00:15:44 -0300

    inicio classe cliente

--- Cliente.java
+++ Cliente.java
@@ -1,6 +1,38 @@
 public class Cliente {
-  public static void main(String[] args) {
-    
-    
+
+    public int id;
+    public String nome;
+    public String cpf;
+    public String cidade;
+
+  public String getNome(){
+    return nome;
+
   }
+
+  public String getCpf(){
+    return cpf;
+
+  }
+  public String getCidade(){
+    return cidade;
+
+  }
+
+  public void setNome(String nomeAtributo) {
+    this.nome = nomeAtributo;
+  }
+
+  public void setCpf(String cpfAtributo) {
+    this.cpf = cpfAtributo;
+  }
+
+  public void setCidade(String cidadeAtributo) {
+    this.cidade = cidadeAtributo;
+  }
+
+
+
+    
+  
 }
